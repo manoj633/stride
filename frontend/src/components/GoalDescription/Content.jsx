65d503e018e3f7f1398073f4b3b45c40f27500dc
Manoj <manoj09633@gmail.com>
2024-11-07 11:30:39 +0530

    created main content component

---
+++ frontend/src/components/GoalDescription/Content.jsx
@@ -0,0 +1,46 @@
+// Content.jsx
+import React from "react";
+import InfoSection from "./InfoSection.jsx";
+import ProgressChart from "./ProgressChart.jsx";
+import Collaborators from "./Collaborators.jsx";
+import Dependencies from "./Dependencies.jsx";
+import TaskSection from "./TaskSection.jsx";
+import RelatedGoals from "./RelatedGoals.jsx";
+import CommentsSection from "./CommentsSection.jsx";
+
+const Content = ({
+  goal,
+  tags,
+  tasks,
+  collaborators,
+  dependencies,
+  relatedGoals,
+  comments,
+  comment,
+  onAddComment,
+  setComment,
+  onRemoveTag,
+  onAddTag,
+}) => (
+  <div className="goal-description__content">
+    <InfoSection
+      goal={goal}
+      tags={tags}
+      onRemoveTag={onRemoveTag}
+      onAddTag={onAddTag}
+    />
+    <ProgressChart goal={goal} />
+    <Collaborators collaborators={collaborators} />
+    <Dependencies dependencies={dependencies} />
+    <TaskSection tasks={tasks} />
+    <RelatedGoals relatedGoals={relatedGoals} />
+    <CommentsSection
+      comments={comments}
+      comment={comment}
+      onAddComment={onAddComment}
+      setComment={setComment}
+    />
+  </div>
+);
+
+export default Content;
