0b69df72d956a2213d10438dbb9b2779724c82d6
Manoj <manoj09633@gmail.com>
2024-11-08 09:38:47 +0530

    added routes to get individual goal, task and subtask and also in bulk

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,41 @@
 import express from "express";
+import goals from "./data/goals.js";
+import tasks from "./data/tasks.js";
+import subtasks from "./data/subtasks.js";
 
 const port = 5000;
 const app = express();
 app.get("/", (req, res) => {
   res.send("API is running");
+});
+
+app.get("/api/goals", (req, res) => {
+  res.json(goals);
+});
+
+app.get("/api/goals/:id", (req, res) => {
+  const goal = goals.find((g) => {
+    return g._id === req.params.id;
+  });
+  res.json(goal);
+});
+
+app.get("/api/tasks", (req, res) => {
+  res.json(tasks);
+});
+
+app.get("/api/tasks/:id", (req, res) => {
+  const task = tasks.find((t) => t._id === req.params.id);
+  res.json(task);
+});
+
+app.get("/api/subtasks/:id", (req, res) => {
+  const subtask = subtasks.find((s) => s._id === req.params.id);
+  res.json(subtask);
+});
+
+app.get("/api/subtasks", (req, res) => {
+  res.json(subtasks);
 });
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
