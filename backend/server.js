c0af5ef8a77e277a0a85165636f91cce4a493c15
Manoj <manoj09633@gmail.com>
2024-11-09 16:23:50 +0530

    modified server to use goals routes

--- backend/server.js
+++ backend/server.js
@@ -2,49 +2,23 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 
-import goals from "./data/goals.js";
-import tasks from "./data/tasks.js";
-import subtasks from "./data/subtasks.js";
 import connectDB from "./config/db.js";
 
-console.log(process.env.PORT);
+import goalRoutes from "./routes/goalRoutes.js";
+
 const port = process.env.PORT || 5000;
 
 connectDB();
 
 const app = express();
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
+app.use(express.json());
 
-app.get("/api/goals", (req, res) => {
-  res.json(goals);
-});
-
-app.get("/api/goals/:id", (req, res) => {
-  const goal = goals.find((g) => {
-    return g._id === req.params.id;
+app
+  .listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  })
+  .on("error", (err) => {
+    console.error("Failed to start server:", err);
   });
-  res.json(goal);
-});
 
-app.get("/api/tasks", (req, res) => {
-  res.json(tasks);
-});
-
-app.get("/api/tasks/:id", (req, res) => {
-  const task = tasks.find((t) => t._id === req.params.id);
-  res.json(task);
-});
-
-app.get("/api/subtasks/:id", (req, res) => {
-  const subtask = subtasks.find((s) => s._id === req.params.id);
-  res.json(subtask);
-});
-
-app.get("/api/subtasks", (req, res) => {
-  res.json(subtasks);
-});
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+app.use("/api/goals", goalRoutes);
