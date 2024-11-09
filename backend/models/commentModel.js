c1f19623f1f8c499402af2f9c04cb863d3d59545
Manoj <manoj09633@gmail.com>
2024-11-09 13:37:35 +0530

    created comment schema and model

---
+++ backend/models/commentModel.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const CommentSchema = new mongoose.Schema(
+  {
+    goalId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Goal",
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+    authorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Comment = mongoose.model("Comment", CommentSchema);
+export default Comment;
