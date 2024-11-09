74190f26adc1df4261871371da03fb1e76946f42
Manoj <manoj09633@gmail.com>
2024-11-09 16:42:00 +0530

    modified goal router to only have routing logic

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,28 +1,10 @@
 import express from "express";
-import asyncHandler from "../middleware/asyncHandler.js";
-import Goal from "../models/goalModel.js";
+import { getGoalById, getGoals } from "../controllers/goalController.js";
 
 const router = express.Router();
 
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const goals = await Goal.find({});
-    res.json(goals);
-  })
-);
+router.route("/").get(getGoals);
 
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const goal = await Goal.findById(req.params.id);
-    if (goal) {
-      return res.json(goal);
-    }
-
-    res.status(404);
-    throw new Error("Resource not found");
-  })
-);
+router.route("/:id").get(getGoalById);
 
 export default router;
