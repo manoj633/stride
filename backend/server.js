70e1990a17e2dd7f2969b9411208538a78265567
Manoj <manoj09633@gmail.com>
2024-11-11 12:08:44 +0530

    Added cors to prevent cross-origin issue

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
@@ -15,6 +16,7 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/goals", goalRoutes);
