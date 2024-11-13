8c6974a584f1e748f108366456fd9ce6e9cee2b1
Manoj <manoj09633@gmail.com>
2024-11-14 00:09:27 +0530

    added winston to server js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import winston from "winston"; // Import Winston
 
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
-
 import goalRoutes from "./routes/goalRoutes.js";
 import taskRoutes from "./routes/taskRoutes.js";
 import subtaskRoutes from "./routes/subtaskRoutes.js";
@@ -19,6 +19,34 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Configure Winston logger
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.simple()
+  ),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: "server.log" }),
+  ],
+});
+
+// Use Winston to log server start
+app
+  .listen(port, () => {
+    logger.info(`Server running on port ${port}`);
+  })
+  .on("error", (err) => {
+    logger.error("Failed to start server:", err);
+  });
+
+// Use Winston to log errors
+app.use((err, req, res, next) => {
+  logger.error(err.stack);
+  next(err);
+});
+
 app.use("/api/goals", goalRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/subtasks", subtaskRoutes);
@@ -26,11 +54,3 @@ app.use("/api/tags", tagRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
-
-app
-  .listen(port, () => {
-    console.log(`Server running on port ${port}`);
-  })
-  .on("error", (err) => {
-    console.error("Failed to start server:", err);
-  });
