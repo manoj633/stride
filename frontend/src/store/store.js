5e3227ddbfe1f3b131115cfcb66b5bc5715534ca
Manoj <manoj09633@gmail.com>
2024-11-13 11:40:35 +0530

    added goal reducer to store

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,10 +1,12 @@
 // store/store.js
 import { configureStore } from "@reduxjs/toolkit";
 import tagReducer from "./features/tags/tagSlice";
+import goalReducer from "./features/goals/goalSlice";
 
 export const store = configureStore({
   reducer: {
     tags: tagReducer,
+    goals: goalReducer,
     // Future reducers can be added here (e.g., goals)
   },
 });
