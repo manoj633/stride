d9074739fab53432bb3ea8acd85f30a971ca0319
Manoj <manoj09633@gmail.com>
2024-11-23 23:45:34 +0530

    fixed frontend build issue

--- backend/server.js
+++ backend/server.js
@@ -61,5 +61,17 @@ app.use("/api/subtasks", subtaskRoutes);
 app.use("/api/tags", tagRoutes);
 app.use("/api/comments", commentRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
+
 app.use(notFound);
 app.use(errorHandler);
