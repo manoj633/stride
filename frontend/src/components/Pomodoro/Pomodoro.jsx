aad829c41a9be61b4d0495bca22914d92f4341d9
Manoj <manoj09633@gmail.com>
2025-01-16 08:53:02 +0530

    Refactored Pomodoro

--- frontend/src/components/Pomodoro/Pomodoro.jsx
+++ frontend/src/components/Pomodoro/Pomodoro.jsx
@@ -1,16 +1,11 @@
 import React, { useContext, useState } from "react";
 import TimerCard from "./TimerCard";
 import "./Pomodoro.css"; // Import a CSS file for styling this component
-import { TimerContext } from "./TimerContext";
 
 const Pomodoro = () => {
-  const [pomodorosCompleted, setPomodorosCompleted] = useState(0);
-  const { activeTimer } = useContext(TimerContext);
   return (
     <div className="pomodoro-container">
-      <TimerCard
-        onPomodoroComplete={() => setPomodorosCompleted(pomodorosCompleted + 1)}
-      />
+      <TimerCard />
     </div>
   );
 };
